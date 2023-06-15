f08db25395482b96d5d6e6b7365b9329e9512822
Khaled Sakr <k.sakr@impargo.de>
2023-06-15 09:32:43 +0200

    feat: update map style to remove timed restrictions

--- src/useVectorLayers.ts
+++ src/useVectorLayers.ts
@@ -1,5 +1,35 @@
 import { useEffect } from "react";
 
+const updateStyle = (map: H.Map) => {
+  // get the vector provider from the base layer
+  // @ts-ignore
+  const provider = map.getBaseLayer().getProvider();
+
+  // get the style object for the base layer
+  const truckStyle = provider.getStyle();
+
+  const changeListener = () => {
+    // @ts-ignore
+    if (truckStyle.getState() === H.map.Style.State.READY) {
+      truckStyle.removeEventListener("change", changeListener);
+
+      truckStyle.setProperty("textures.truck-attributes.sprites.truck-attributes--weight_timed", "None");
+      truckStyle.setProperty("textures.truck-attributes.sprites.truck-attributes--width_timed", "None");
+      truckStyle.setProperty("textures.truck-attributes.sprites.truck-attributes--wpa_timed", "None");
+      truckStyle.setProperty("textures.truck-attributes.sprites.truck-attributes--height_timed", "None");
+      truckStyle.setProperty("textures.truck-attributes.sprites.truck-attributes--length_timed", "None");
+      truckStyle.setProperty("textures.truck-attributes.sprites.truck-attributes--default_timed", "None");
+      truckStyle.setProperty("textures.truck-attributes.sprites.truck-attributes--trailers_timed", "None");
+      const lineFilters = truckStyle.getProperty("layers.truck-restriction.lines.filter.all");
+      truckStyle.setProperty("layers.truck-restriction.lines.filter.all", [...lineFilters, {
+        hgv_time_restrictions: false,
+      }]);
+    }
+  };
+
+  truckStyle.addEventListener("change", changeListener);
+};
+
 export interface UseVectorLayersProps {
   map?: H.Map;
   truckRestrictions?: boolean;
@@ -21,11 +51,13 @@ export const useVectorLayers = ({
   defaultLayers,
   useVectorTiles,
 }: UseVectorLayersProps) => {
+
   useEffect(() => {
     if (map && defaultLayers && useVectorTiles) {
       if (truckRestrictions) {
         // @ts-ignore
         map.setBaseLayer(defaultLayers.vector.normal.truck);
+        updateStyle(map);
       } else {
         map.setBaseLayer(useSatellite
           ? defaultLayers.raster.satellite.map
